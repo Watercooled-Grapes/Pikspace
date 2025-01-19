ddff71d5a901ea37f1d39b869b40eb0ddbd00f10
axiev1 <150072464+axiev1@users.noreply.github.com>
2025-01-19 01:16:32 -0800

    merged shit

---
+++ Assets/PhotoBookScript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoBookScript : MonoBehaviour
+{
+    [SerializeField] private GameObject photoManager;
+    PhotoManagerScript photoManagerScript;
+    private RenderTexture photo;
+    private int idx = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        photoManagerScript = photoManager.GetComponent<PhotoManagerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void updatePhoto()
+    {
+        photo = photoManagerScript.getPhoto(idx);
+        GetComponent<RawImage>().texture = photo;
+    }
+
+    public void nextPhoto()
+    {
+        idx = Mathf.Min(photoManagerScript.getSize() - 1, idx + 1);
+        updatePhoto();
+    }
+
+    public void prevPhoto()
+    {
+        idx = Mathf.Max(0, idx - 1);
+        updatePhoto();
+    }
+
+    public void resetPhoto()
+    {
+        idx = photoManagerScript.getSize();
+        updatePhoto();
+    }
+}
---
+++ Assets/PhotoManagerScript.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PhotoManagerScript : MonoBehaviour
+{
+    private List<RenderTexture> photoData;
+    [SerializeField] private GameObject photoDisplay;
+    [SerializeField] private Camera handheldCamera;
+    bool triggerValue;
+
+
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
+    {
+        photoData = new List<RenderTexture>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public int getSize()
+    {
+        return photoData.Count;
+    }
+
+    public void addPhoto(RenderTexture photo)
+    {
+        photoData.Add(photo);
+    }
+
+    public RenderTexture getPhoto()
+    {
+        return photoData.ElementAt(getSize());
+    }
+
+    public RenderTexture getPhoto(int index)
+    {
+        if (index < 0 || index >= getSize())
+        {
+            return null;
+        }
+
+        return photoData.ElementAt(index);
+    }
+
+    public void takePhoto()
+    { 
+        RenderTexture photoTexture = new RenderTexture(handheldCamera.targetTexture.width, handheldCamera.targetTexture.width, 16, handheldCamera.targetTexture.graphicsFormat);
+        handheldCamera.Render();
+        Graphics.CopyTexture(handheldCamera.targetTexture, photoTexture);
+        addPhoto(photoTexture);
+        photoDisplay.GetComponent<PhotoBookScript>().nextPhoto();
+    }
+}
